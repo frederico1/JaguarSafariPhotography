f5128d4d5e2c6237a5f8dea27242029dc5d9670f
José Antonio <89794419+frederico1@users.noreply.github.com>
2023-11-23 15:21:43 -0400

    fonts

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
 </script>
 <style>
 #app {
-  font-family: "Nunito Variable", sans-serif;
+  font-family: "Actor", sans-serif;
   margin: 0;
   padding: 0;
   border: 0;
--- src/main.js
+++ src/main.js
@@ -5,7 +5,16 @@ import router from "./router";
 import store from "./store";
 import "./assets/style.css";
 import "@fontsource-variable/nunito";
+import "@fontsource-variable/montserrat";
 import "@mdi/font/css/materialdesignicons.css";
+import "@fontsource-variable/dosis";
+import "@fontsource/merriweather";
+import "@fontsource-variable/literata";
+import "@fontsource-variable/assistant";
+import "@fontsource/didact-gothic";
+import "@fontsource-variable/league-spartan";
+import "@fontsource/alegreya-sans";
+import "@fontsource/actor";
 // Vuetify
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
