e8fe50fa3d31d6d19c2845f06ae36c0f115e70f3
JOSE ANTONIO FREDERICO ALBERNAZ DA CRUZ <jose.cruz@hmsm.com.br>
2023-11-25 21:58:23 -0400

    .

--- src/views/TourFourNights.vue
+++ src/views/TourFourNights.vue
@@ -240,27 +240,61 @@
 
           <!-- FOOTER -->
 
-          <div class="mt-10">
+          <div class="mt-10" data-aos="fade-down">
             <!-- ICONES -->
             <div class="flex justify-center items-center gap-5">
               <div class="border-b-2 h-8 border-[#E1306C]">
-                <Icon icon="iconoir:instagram" width="27" height="27" />
+                <a
+                  href="https://instagram.com/guitophotographs"
+                  target="_blanck"
+                >
+                  <Icon icon="iconoir:instagram" width="27" height="27" />
+                </a>
               </div>
 
               <div class="border-b-2 h-8 border-[#128C7E]">
-                <Icon icon="la:whatsapp" width="27" height="27" />
+                <a
+                  href="https://api.whatsapp.com/send?phone=+5565992764968&text=Hello%21"
+                  target="_blanck"
+                >
+                  <Icon icon="la:whatsapp" width="27" height="27" />
+                </a>
               </div>
 
               <div class="border-b-2 h-8 border-[#25D366]">
-                <Icon icon="cib:tripadvisor" color="" width="27" height="27" />
+                <a href="https://www.tripadvisor.com.br" target="_blanck">
+                  <Icon
+                    icon="cib:tripadvisor"
+                    color=""
+                    width="27"
+                    height="27"
+                  />
+                </a>
+              </div>
+
+              <div>
+                <div class="border-b-2 h-8 border-[#4285F4]">
+                  <a
+                    href="mailto:http://jaguarsafariphotograph@gmail.com"
+                    target="_blanck"
+                  >
+                    <Icon icon="iconamoon:email-thin" width="27" height="27" />
+                  </a>
+                </div>
               </div>
             </div>
+          </div>
 
-            <div
-              class="mt-6 uppercase text-xs flex flex-col justify-center items-center"
-            >
+          <!-- FOOTER FOOTER -->
+          <div
+            class="mt-6 uppercase text-xs flex flex-col justify-center items-center tracking-wider"
+          >
+            <div class="">
               <p>© 2023 All rights reserved.</p>
-              <p>Jaguar Safari Photography</p>
+            </div>
+
+            <div>
+              <p class="">Jaguar Safari Photography</p>
             </div>
           </div>
           <div></div>
