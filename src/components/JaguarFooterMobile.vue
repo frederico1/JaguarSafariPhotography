c15db851e1d0a350f58ad8c167330b536113ebe8
José Antonio <ja.frederico1@gmail.com>
2024-01-09 10:32:27 -0300

    border bottom icons

--- src/components/JaguarFooterMobile.vue
+++ src/components/JaguarFooterMobile.vue
@@ -5,14 +5,14 @@
   <div class="mt-10">
     <!-- ICONES -->
     <div class="flex justify-center items-center gap-5">
-      <div class="">
+      <div class="" data-aos="fade-right" data-aos-delay="50">
         <a href="https://instagram.com/guitophotographs" target="_blanck">
           <Icon icon="iconoir:instagram" width="28" height="28" />
         </a>
         <div class="mt-1 h-[2px] bg-[#E1306C] w-4/4 flex justify-center items-center mx-auto "></div>
       </div>
 
-      <div class="border-b-2 h-8 border-[#128C7E] ">
+      <div class="" data-aos="fade-down" data-aos-delay="50">
         <a
           href="https://api.whatsapp.com/send?phone=+5565992764968&text=Hello%21"
           target="_blanck"
@@ -22,7 +22,7 @@
         <div class="mt-1 h-[2px] bg-[#128C7E] w-4/4 flex justify-center items-center mx-auto "></div>
       </div>
 
-      <div class="border-b-2 h-8 border-[#25D366]">
+      <div class="" data-aos="fade-up" data-aos-delay="50">
         <a href="https://www.tripadvisor.com.br/Profile/jaguarsafarip" target="_blanck">
           <Icon icon="cib:tripadvisor" color="" width="28" height="28" />
         </a>
@@ -30,7 +30,7 @@
       </div>
       
       <div>
-        <div class="border-b-2 h-8 border-[#4285F4]">
+        <div class="" data-aos="fade-left" data-aos-delay="50">
           <a
             href="mailto:http://jaguarsafariphotograph@gmail.com"
             target="_blanck"
