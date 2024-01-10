7c3a449834740c866dfc7115a4d412fd00bf7804
JOSE ANTONIO FREDERICO ALBERNAZ DA CRUZ <jose.cruz@hmsm.com.br>
2024-01-10 18:20:50 -0400

    .

--- src/views/TourSixNights.vue
+++ src/views/TourSixNights.vue
@@ -16,7 +16,7 @@
           @click="scrollToTop"
           v-show="showBackToTopButton"
         >
-          <Icon icon="tdesign:arrow-up" color="#000" width="33px" />
+          <Icon icon="tdesign:arrow-up" color="#000" width="34px" />
         </button>
       </div>
 
